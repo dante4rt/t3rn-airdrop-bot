fda092997623291c5b681ae32e501e59b3f65663
Ope <hello.ope21@gmail.com>
2024-09-14 19:01:07 +0700

    fix error writing log when there is no file at first run

--- utils/log.js
+++ utils/log.js
@@ -1,6 +1,24 @@
 const fs = require('fs');
 
 function writeLog(filePath, newText) {
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            // File does not exist. Creating new file
+            fs.writeFile(filePath, '', (err) => {
+                if (err) {
+                    console.error('Error creating the file:', err);
+                    return;
+                }
+
+                appendTextToFile(filePath, newText); 
+            });
+        } else {
+            appendTextToFile(filePath, newText);
+        }
+    });
+}
+
+function appendTextToFile(filePath, newText) {
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             console.error('Error reading the file:', err);
@@ -12,8 +30,6 @@ function writeLog(filePath, newText) {
         fs.writeFile(filePath, updatedData, (err) => {
             if (err) {
                 console.error('Error writing to the file:', err);
-            } else {
-                console.log('File successfully updated.');
             }
         });
     });
