64259a1f484bfb28be575c8c864b4006dd6839c1
Everest1508 <riteshmahale15@gmail.com>
2025-01-01 16:03:22 +0530

    fix/rpc-url-fix

--- contracts/ABI.js
+++ contracts/ABI.js
@@ -188,7 +188,7 @@ const T3RN_ABI = [
   {
     CA_ARBT: '0x8D86c3573928CE125f9b2df59918c383aa2B514D',
     CA_OPSP: '0xF221750e52aA080835d2957F2Eed0d5d7dDD8C38',
-    RPC_ARBT: 'https://arbitrum-sepolia.blockpi.network/v1/rpc/public',
+    RPC_ARBT: 'https://sepolia-rollup.arbitrum.io/rpc',
     RPC_OPSP: 'https://sepolia.optimism.io',
   },
 ];
